e7a25cba3019e0e9fca30c6aa453b2b827669012
Maksim Efimchick <maksefimchic@gmail.com>
2023-06-16 11:01:04 +0300

    add moderating system

--- src/main/java/com/HarmyFounder/HItxt/models/Permission.java
+++ src/main/java/com/HarmyFounder/HItxt/models/Permission.java
@@ -3,7 +3,8 @@ package com.HarmyFounder.HItxt.models;
 public enum Permission {
     POSTS_READ("posts:read"),
     POSTS_WRITE("posts:write"),
-    POSTS_MODERATE("posts:moderate");
+    POSTS_MODERATE("posts:moderate"),
+    USERS_MODERATE("users:moderate");
     private String permission;
 
     Permission(String permission) {
